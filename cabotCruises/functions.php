e069f1383bc4875d03cb40f83774aed4aab78afb
Unknown <riderjensen@gmail.com>
2018-09-19 14:09:27 -0600

    News Items Fix from Prof Cheney

--- cabotCruises/functions.php
+++ cabotCruises/functions.php
@@ -1,17 +1,17 @@
 <?php
 
 function register_my_menu() {
-	register_nav_menu('header-menu',__( 'Header Menu' ));
-  }
-  add_action( 'init', 'register_my_menu' );
-  remove_filter( 'the_content', 'wpautop' );
+register_nav_menu('header-menu',__( 'Header Menu' ));
+}
+add_action( 'init', 'register_my_menu' );
+remove_filter( 'the_content', 'wpautop' );
 remove_filter( 'the_excerpt', 'wpautop' );
 
 function wpdocs_excerpt_more( $more ) {
-  return sprintf( '<a class="read-more" href="%1$s">%2$s</a>',
-      get_permalink( get_the_ID() ),
-      __( 'Read More', 'textdomain' )
-  );
+return sprintf( '<a class="read-more" href="%1$s">%2$s</a>',
+    get_permalink( get_the_ID() ),
+    __( 'Read More', 'textdomain' )
+);
 }
 add_filter( 'excerpt_more', 'wpdocs_excerpt_more' );
 
